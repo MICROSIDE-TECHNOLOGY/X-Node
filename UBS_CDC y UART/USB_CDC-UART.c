52755b5fe65b4dbd11445bc3855afc52c1692f80
crushcardtopo01 <silentlv5@gmail.com>
2020-11-19 17:39:26 -0600

    Change position CCS examples

--- UBS_CDC y UART/USB_CDC-UART.c
+++ UBS_CDC y UART/USB_CDC-UART.c
@@ -4,15 +4,15 @@
 */
 #include "18f45K50.h" //Para PIC18F4550 cambiar por: #include <18F4550.h>
 #use delay(internal=48000000) // Tipo de oscilador y frecuencia dependiendo del microcontrolador 
-#use rs232(baud=115200, xmit=PIN_C6, rcv=PIN_C7, stream=UART)   //Comunicación serial a 115200bps, 1 bit de paro, sin paridad
+#use rs232(baud=115200, xmit=PIN_C6, rcv=PIN_C7, stream=UART)   //Comunicacion serial a 115200bps, 1 bit de paro, sin paridad
 #zero_ram
-#build(reset=0x02000,interrupt=0x02008) // Asigna los vectores de reset e interrupción para la versión con bootloader
+#build(reset=0x02000,interrupt=0x02008) // Asigna los vectores de reset e interrupcion para la version con bootloader
 #org 0x0000,0x1FFF {} // Reserva espacio en memoria para el bootloader 
 
 //--------------------------------------------------------------------------------------------------------------------
 
-#include <usb_cdc.h> //Librería USB
-#include <usb_desc_cdc.h> //Librería USB
+#include <usb_cdc.h> //Libreria USB
+#include <usb_desc_cdc.h> //Libreria USB
 
 char c,u; //Crea variables tipo char para guardar los datos
 
@@ -21,7 +21,7 @@ void main() {
 usb_cdc_init(); // Configuramos al puerto virtual
 usb_init(); //inicializamos el USB
 
-      while (!usb_cdc_connected()) {} // espera a detectar una transmisión de la PC
+      while (!usb_cdc_connected()) {} // espera a detectar una transmision de la PC
       while (true)
       {
          usb_task();
@@ -30,13 +30,13 @@ usb_init(); //inicializamos el USB
             if (usb_cdc_kbhit())
             {
                c = usb_cdc_getc(); //Espera a recibir un datos por el puerto USB
-               putc(c,UART); //Envía la información por UART
+               putc(c,UART); //Envia la informacion por UART
             }
 
             if (kbhit(UART))
             {
-               u=getc(UART); //Recibe información por UART
-               usb_cdc_putc(u); //Envia información hacia la computadora por puerto emulado USB
+               u=getc(UART); //Recibe informacion por UART
+               usb_cdc_putc(u); //Envia informacion hacia la computadora por puerto emulado USB
             }
          }
       }
